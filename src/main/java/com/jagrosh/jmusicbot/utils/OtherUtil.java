c3ab125bedb5c7387747128eea29ad92803d7d02
웹통신위원회 <41885157+INMD1@users.noreply.github.com>
2020-08-01 18:48:11 +0900

    0.1.1 문장수정

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -170,7 +170,7 @@ public class OtherUtil
         if(JMusicBot.class.getPackage()!=null && JMusicBot.class.getPackage().getImplementationVersion()!=null)
             return JMusicBot.class.getPackage().getImplementationVersion();
         else
-            return "0.1.0";
+            return "0.1.1";
     }
     
     public static String getLatestVersion()
